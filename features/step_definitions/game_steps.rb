8ec65c9fe618e1171df8d79ee2ffa6071fe0172a
Julio Lucero <poramo@gmail.com>
2012-10-17 16:10:53 -0300

    first escenario

---
+++ features/step_definitions/game_steps.rb
@@ -0,0 +1,7 @@
+Given /^inicio el juego$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see 'A B C'$/ do
+  pending # express the regexp above with the code you wish you had
+end
---
+++ features/tablero.feature
@@ -0,0 +1,5 @@
+Feature: tablero
+
+  Scenario: Tablero Base
+    Given inicio el juego
+    Then I should see 'A B C'
